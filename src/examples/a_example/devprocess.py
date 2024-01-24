53b0b46c5dcb4fd942412c899a908bc486a3bbbb
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-24 02:38:57 +0000

    hello world program

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -5,3 +5,6 @@ def add_numbers(val1, val2): #defining a function - python code that handles one
     another comment
     '''
     return val1 - val2
+
+def say_hello(name):
+    print("Hello World! " + name)
