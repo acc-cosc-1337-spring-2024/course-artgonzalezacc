4df9aac6d8b5dfd278cc7b957dcaf215833ec70f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-24 02:27:41 +0000

    dev process, code and test , use in main

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -1,3 +1,7 @@
-def add_numbers(val1, val2):
-     '''Type the correct code to add two numbers after the return statement.'''
-     return val1 + val2
+def add_numbers(val1, val2): #defining a function - python code that handles one task
+    #comment
+    '''
+    one line comment
+    another comment
+    '''
+    return val1 - val2
--- tests/examples/a_example/tests_devprocess.py
+++ tests/examples/a_example/tests_devprocess.py
@@ -4,6 +4,6 @@ from src.examples.a_example.devprocess import add_numbers
 
 class Test_Config(unittest.TestCase):
 
-    def test_add_numbers_2(self):
+    def test_add_numbers_2(self):#create one test function-task=test that add numbers works as expected
         self.assertEqual(2, add_numbers(1, 1))
 
