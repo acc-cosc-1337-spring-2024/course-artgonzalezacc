d2ac3fddb3c27cd3ee1e52a605350e8708ab01dc
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-26 02:19:11 +0000

    int, float, and string variables

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,16 @@ def display_output():
 def test_config():
     return True
 
+def echo_value(value):
+    print(value)
+
+def say_hello(name):
+    #sequence of characters -- string 
+    display_value = "Hello " + name #display_value will hold the value of Hello Python(if the value that the name variables holds is Python)
+    print(display_value)
+
+def add_values(num1, num2):
+    result = num1 + num2 #doing calculations/ return the value to make the function testable
+
+    return result
+
