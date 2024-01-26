459836ece8f59f85d341aad4215eb212c1b22f91
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-01-26 02:32:13 +0000

    intro to input w int, float arithmetic ops, and strings

--- src/examples/b_input_proc_output/main.py
+++ src/examples/b_input_proc_output/main.py
@@ -1,20 +1,14 @@
 import input_process_output
 
-input_process_output.display_output()
-
-input_process_output.echo_value("Python")
-input_process_output.echo_value("C++")
-
-input_process_output.say_hello("Python learners")
-
-#create variable in the main.py file(program) but use it in functions
-result = input_process_output.add_values(5, 5) #create a whole number variable
+value = input("Enter value: ")
+result = input_process_output.add_values(value, value) #treats the value variable as a string
 print(result)
 
-result = 10.99
-result = input_process_output.add_values(result, 1.50)
+
+value = input("Enter value: ") #reads values from keyboard as string(alphanumeric)
+result = input_process_output.add_values(int(value), int(value)) #convert alpha to int(whole number) or float(decimal number)
 print(result)
 
-result = "10.99"
-result = input_process_output.add_values(result, "1.50")
+value = input("Enter value: ") #reads values from keyboard as string(alphanumeric)
+result = input_process_output.add_values(float(value), float(value)) #convert alpha to int(whole number) or float(decimal number)
 print(result)
