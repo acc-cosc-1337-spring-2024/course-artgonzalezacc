317f8ed38532deb96c0197dcfb97a2ba505873f9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-07 02:03:40 +0000

    intro to if else statement in main

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -5,8 +5,12 @@ result = decisions.is_even(num)
 
 if(result == True):
     print("Even ", result)
+else:
+    print("Number is not Even")
 
 result = decisions.is_odd(num)
 
 if(result == True):
     print("Odd ", result)
+else:
+    print("Number is not Odd")
