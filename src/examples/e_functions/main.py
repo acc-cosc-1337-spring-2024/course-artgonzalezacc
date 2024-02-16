bb2bb84f66ec23681503bc79fa0a6be4925ec263
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-16 02:43:59 +0000

    value return function variables/params

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,12 +1,4 @@
 #main program
-import void_functions
+import value_return_functions
 
-#void_functions.use_global_variable()
-#void_functions.use_global_variable_1()
-#void_functions.try_change_global_variable()
-#void_functions.change_global_variable()
-
-void_functions.use_global_variable_w_param(10)
-print("Global: ", void_functions.val2)
-
-#void_functions.use_global_variable()
+value_return_functions.main()
--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -1,2 +1,12 @@
 def test_config():
     return True
+
+def get_product(num1, num2):
+    return num1 * num2
+
+def main():
+    num1 = 5
+    num2 = 10
+    result = get_product(num1, num2)
+
+    print(result)
