67b7a24af7522aa390ff1022d2cea31183023c65
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-16 02:30:25 +0000

    global variables

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,4 +1,12 @@
 #main program
 import void_functions
 
-void_functions.say_hello("Python")
+#void_functions.use_global_variable()
+#void_functions.use_global_variable_1()
+#void_functions.try_change_global_variable()
+#void_functions.change_global_variable()
+
+void_functions.use_global_variable_w_param(10)
+print("Global: ", void_functions.val2)
+
+#void_functions.use_global_variable()
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -6,3 +6,24 @@ def say_hello(name): #name - is a parameter (variable)
 def hello_world():
     val = "Hello World" #local function variables
     print(val)
+
+val2 = 5 #global variables
+
+def use_global_variable():
+    print(val2)
+
+def use_global_variable_1():
+    print(val2)
+
+def try_change_global_variable():
+    val2 = 10
+    print("Local: ", val2)
+
+def change_global_variable():
+    global val2 #tell python we want to use the val2 global variable
+    val2 = 10
+    print(val2)
+
+def use_global_variable_w_param(val2):
+    val2 = 10
+    print("Local: ", val2)
