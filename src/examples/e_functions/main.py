fa143410c82e4260546618e6edcae94824778d10
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-16 01:58:34 +0000

    void and function variables/parameters

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,1 +1,4 @@
 #main program
+import void_functions
+
+void_functions.say_hello("Python")
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -1,1 +1,8 @@
 #
+def say_hello(name): #name - is a parameter (variable)
+    val = "C++"
+    print("Hello " + name + " " + val)
+
+def hello_world():
+    val = "Hello World" #local function variables
+    print(val)
