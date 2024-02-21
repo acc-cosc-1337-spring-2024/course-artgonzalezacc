33d3a6da7e9f95a4db74ba43adae6e23aed10b53
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-21 02:03:49 +0000

    loop string w while

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,7 +1,4 @@
 #
+import strings
 lang = "C++" #string-sequence of characters
-print(lang[0])
-print(lang[1])
-print(lang[2])
-#print(lang[3]) -- try to access an out of range index
-print(len(lang))
+strings.loop_string_w_while(lang)
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,9 @@
 #
+def loop_string_w_while(str):
+    index = 0
+    size = len(str) #size of the string
+
+    while(index < size):
+        print(str[index])
+        index += 1
+
