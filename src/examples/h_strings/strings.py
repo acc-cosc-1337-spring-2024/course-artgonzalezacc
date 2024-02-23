9d3fd5f777c72c7e5214b356064e0ad561ed5728
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-02-23 01:37:42 +0000

    string passed as value parameter

--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -7,3 +7,6 @@ def concat_string(str1, str2):
 def concat_string_w_plus_equal(str1, str2):
     str1 += str2 #str1 = str1 + str2
     return str1
+
+def string_params(str):
+    str = "Python"
--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.h_strings.strings import concat_string, concat_string_w_plus_equal, test_config
+from src.examples.h_strings.strings import concat_string, concat_string_w_plus_equal, string_params, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -12,5 +12,10 @@ class Test_Config(unittest.TestCase):
 
     def test_string_concat_w_plus_equal(self):
         self.assertEqual(concat_string_w_plus_equal("C++ ", "cosc 1337"), "C++ cosc 1337")
+
+    def test_string_params_modify(self):
+        lang = "C++"
+        string_params(lang)
+        self.assertEqual(lang, "C++")
         
 
