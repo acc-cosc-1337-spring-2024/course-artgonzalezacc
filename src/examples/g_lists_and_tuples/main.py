2d291b711335dcc3a1acf909b6b5b49e4d58d140
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-06 01:58:14 +0000

    tuples

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,4 +1,17 @@
 #main program
 import lists
 
-lists.two_dimensional_list()
+my_read_only_list = (1,2,3,4) #tuple
+
+print(my_read_only_list)
+
+print(my_read_only_list[2])
+
+if 4 in my_read_only_list:
+    print('4 exists')
+
+index = my_read_only_list.index(2)
+
+print(index)
+
+print(len(my_read_only_list))
