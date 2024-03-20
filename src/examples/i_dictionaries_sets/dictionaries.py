e588f2299f3cd0d5525b481ebd476e8d6e28d439
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-20 01:49:39 +0000

    create dictionary

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,2 +1,7 @@
 def test_config():
     return True
+
+def create_dictionary():
+    prog_langs = {'C++':'1979', 'Java':'1992', 'Python':'1996', 'C#':'2001'}
+    print(prog_langs['C++'])
+    print(prog_langs['C#'])
