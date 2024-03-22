62e557d16ea1b855c9230b1c4be7a41b1855a348
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-03-22 00:52:29 +0000

    search dictionary with not in

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -14,5 +14,13 @@ def find_key_in_dictionary_with_in(key, prog_langs):
     else:
         print(key, "key not in dictionary")
 
+def find_key_dictionary_with_not_in(key, prog_langs):
+    
+    if key not in prog_langs:
+        print(key, "key not in dictionary")
+    else:
+        print(prog_langs[key])
+        
+
 
 
--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -5,4 +5,4 @@ prog_langs = {'C++':'1979', 'Java':'1992', 'Python':'1996', 'C#':'2001'}
 
 key = input("Enter key: ")
 
-dictionaries.find_key_in_dictionary_with_in(key, prog_langs)
+dictionaries.find_key_dictionary_with_not_in(key, prog_langs)
